c6c95e1371c97b6d351f42f704c86ea92d97457e
mzonerz <mzonerz@gmail.com>
2016-09-05 16:48:30 +0300

    Initial Commit

---
+++ iHelperClasses/IhelperTesterConsoele/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iHelperClasses;
+namespace IhelperTesterConsoele
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            iHelperClasses.ErrorLogger.ErrorWriter("Testloger");
+
+           string password= Console.ReadLine();
+           Console.WriteLine("Entered Password"+ password);
+           string textencrypt= iHelperClasses.Cryptography.Encrypt(password);
+           Console.WriteLine("Password After encrypt:"+textencrypt);
+           string decrypt=iHelperClasses.Cryptography.Decrypt(textencrypt);
+           Console.WriteLine("Password After Decrypt:" + decrypt);
+           Console.ReadKey();
+        }
+    }
+}
---
+++ iHelperClasses/iHelperClasses/ErrorLoger.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace iHelperClasses
+{
+    public class ErrorLogger
+    {
+
+        /// <summary>
+        /// Please use below setting on app.config or web.config for logfile path(Create Log folder manualy if not exist
+        /// 
+        /// <appsetting>
+        ///  <add key="LogFilePath" value="\Log\Log.txt"/>
+        /// </apsetting>
+        /// 
+        /// </summary>
+        /// <param name="Err"></param>
+
+
+
+        public static void ErrorWriter(string Err)
+        {
+            
+                try
+                {
+
+                    if (!String.IsNullOrEmpty(Err)) 
+                    {
+                        string path = ConfigurationManager.AppSettings["LogFilePath"];
+                        Err = Err + "---Start---/n<<Occurred on" + DateTime.Now.ToShortDateString() + "-" + DateTime.Now.ToShortTimeString() + ">>/n---End---";
+                        string relativePath = ConfigurationManager.AppSettings["LogFilePath"];
+                       string vPath = AppDomain.CurrentDomain.BaseDirectory+""+relativePath;
+
+
+
+                        if (File.Exists(vPath))
+                        {
+                            TextWriter tw = new StreamWriter(vPath, true);
+                            tw.WriteLine(Err);
+                            tw.Close();
+                        }
+                        else {
+                            var file = File.CreateText(vPath);
+                            file.Close();
+                                using (StreamWriter writer = new StreamWriter(vPath))
+	                            {
+	                         
+	                                writer.WriteLine(Err);
+                                  
+                                    writer.Flush(); 
+
+	                            }
+                    
+                            
+                        }
+
+                    }
+                }
+                catch (Exception)
+                {
+                    
+                   // throw;
+                }
+              
+            
+        }
+
+    }
+}
